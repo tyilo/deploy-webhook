6a05ab8fc791732a8cfa1ee9a3971c23c066ddfb
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2020-10-12 15:46:39 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import yaml
+
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+
+@app.post("/hook/{token}")
+async def root(token: str):
+    with open("tokens.yml") as f:
+        tokens = yaml.safe_load(f)
+
+    path = tokens.get(token)
+    if not path:
+        raise HTTPException(404, "Matching token does not exist.")
+
+    with open("fifo", "w") as f:
+        f.write(path)
+
+    return {"success": True}
